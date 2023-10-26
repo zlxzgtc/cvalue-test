8d833c088e0e3588fb4e034678ab60eaddc06011
zlx <lexiaozhang@foxmail.com>
2023-10-26 17:11:02 +0800

    useless assignment

--- src/main/java/org/example/meaningless/A.java
+++ src/main/java/org/example/meaningless/A.java
@@ -4,6 +4,10 @@ public class A {
 
     public static void main(String[] args) {
         int a = 1;
+        int b = 1;
+        a = a + b;
+        b = 2;
+        b = 3;
         System.out.println(a);
     }
 }
