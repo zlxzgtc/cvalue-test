f8213bbed80cf4d477ff103b14c9b52d8afb76b8
zlx <lexiaozhang@foxmail.com>
2023-10-26 10:47:29 +0800

    new class A

---
+++ src/main/java/org/example/lomback/A.java
@@ -0,0 +1,37 @@
+package org.example.lomback;
+
+public class A {
+    private int a;
+    private int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public A() {
+        a = 1;
+        b = 2;
+    }
+
+    public A(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
